7cb7f803983f0744879dd00f70b0b0646abf5dde
DaRealPunjabi <shiv.jalli@gmail.com>
2023-04-17 14:47:05 +0100

    Update main.py
    Schema Validation

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from fastapi import Body, FastAPI
+from turtle import title
+from typing import Optional
+#from fastapi import Body, FastAPI
+
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def root():
@@ -11,7 +24,9 @@ def get_posts():
     return {"data": "This is your posts"}
 
 @app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    #return {"message": "successfully created posts"}
-    return {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
+def create_posts(new_post: Post):
+    # print(new_post.published)
+    # print(new_post.rating)
+    print(new_post)
+    print(new_post.dict())
+    return {"data": new_post.dict()}
