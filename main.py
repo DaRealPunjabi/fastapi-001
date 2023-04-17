98725226d03f03ea9a6cfd6a84a90af1de6cf436
DaRealPunjabi <shiv.jalli@gmail.com>
2023-04-17 19:20:57 +0100

    Update main.py
    Creating in Array

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
-
+from random import randrange
 
 app = FastAPI()
 
@@ -28,6 +28,13 @@ def get_posts():
 
 @app.post("/posts")
 def create_posts(post: Post):
-    print(post)
-    print(post.dict())
-    return {"data": post}
+    # print(post)
+    # print(post.dict())
+    print(max([x['id'] for x in my_posts]))
+    post_dict = post.dict()
+    #post_dict['id'] = randrange(1, 10000000)
+    post_dict['id'] = max([x['id'] for x in my_posts]) + 1
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+    # return {"data": post}
