1bdaf5b8ab7abdaa6a57b76d1f69ecde694ed296
DaRealPunjabi <shiv.jalli@gmail.com>
2023-04-28 09:40:20 +0100

    Update main.py
    Retrieve One

--- main.py
+++ main.py
@@ -15,8 +15,15 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+# list of dictionaries
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
             {"title": "favorite foods", "content": "I like pizza", "id": 2}]    
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+            
 
 @app.get("/")
 def root():
@@ -25,6 +32,33 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": my_posts}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    print(id)
+    print(type(id))
+    # try:
+    #     i = int(id)
+    #     print(type(i))
+    # except ValueError as verr:
+    #     print("id does not contain anything convertible to int")
+    # except Exception as ex:
+    #     print("Exception occurred while converting to int")
+    # print(type(i))
+    print(my_posts[1])
+    d = {x['id']: x for x in my_posts}
+    print(d)
+    print('d1')
+    d1 = list(filter(lambda tag: tag['id'] == 2, my_posts))
+    print(d1)
+ 
+    #post =  find_post(int(id))
+    post =  find_post(id)
+    print(post)
+    return {"post_detail": post}
+    #return {"post_detail": f"Here is post {id}"}
+    #return {"post_detail": d1}
+
 
 @app.post("/posts")
 def create_posts(post: Post):
